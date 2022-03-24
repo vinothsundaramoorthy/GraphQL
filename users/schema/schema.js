5d883193223d17b3b41caf68426bf0dfd9f41322
Vinoth S <vinoth.s@thinkpalm.com>
2022-03-24 16:09:40 +0530

    GraphQLwith Express and rootquery

---
+++ users/schema/schema.js
@@ -0,0 +1,35 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Vinoth', age: 29 },
+    { id: '47', firstName: 'Sun', age: 57 }
+]
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return _.find(users, {id: args.id});
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
